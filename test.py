1c5696258f9140be6532aa72932962d95d250d61
LaPluses <xipersup@gmail.com>
2018-04-30 16:04:57 +0800

    modify

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def test_judge_from_file():
         submission = Submission(
             submission = 4 ,
             language = 'GNU G++17',
-            code = open( 'testcase/good_cpp.cpp' , "r" ).read(),
+            code = open( 'testcase/good.cpp' , "r" ).read(),
             sourcefile = 'main-1',
             time_limit = 5000,
             memory_limit = 64,
--- testcase/good.cpp
+++ testcase/good.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 
 int main( int argc , char * argv[] ){
-    sleep( 4 );
     int a , b;
     scanf( "%d%d" , & a , & b );
     printf( "%d\n" , a + b );
